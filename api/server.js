d501763b2a5fad90b0dd6c1cf1d8332a21af4fbd
YuhiTTo <unulucero@gmail.com>
2024-06-23 02:55:00 -0500

    agregacion de lo demas al server

--- api/server.js
+++ api/server.js
@@ -63,7 +63,7 @@ server.get('/usuario/:id', (req, res) => {
   });
   
 server.use(router);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4200;
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
